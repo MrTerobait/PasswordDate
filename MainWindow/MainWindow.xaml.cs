0bc58359d0a03dd7e5582506051f1765b8e44daa
MrTerobait <proger2022@mail.ru>
2021-03-18 00:00:42 +0300

    recordings editors was completed

--- MainWindow/MainWindow.xaml.cs
+++ MainWindow/MainWindow.xaml.cs
@@ -14,7 +14,6 @@ namespace MainWindow
         Opened,
         Closure
     }
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -22,24 +21,31 @@ namespace MainWindow
     {
         private BindingList<Recording> recordingList = new BindingList<Recording>();
         private BindingList<Recording> deletedRecorgingList = new BindingList<Recording>();
-        private int chosenRecordingNumber = -1;
-        private event Action recordingIsChosen;
+        enum OpeningList
+        {
+            RecordingList,
+            DeletedRecordingList
+        }
         private RecordingEditor recordingEditor;
+        private int chosenRecordingNumber = -1;
         public DataWindow()
         {
             InitializeComponent();
         }
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            RecordingDisplayer.ItemsSource = recordingList;
+            SetRecordingButtons();
+            RecordingsDisplayer.ItemsSource = recordingList;
             recordingList.ListChanged += ResetRecordingButtons;
+        }
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            passwordGenerator?.Close();
         }
         private void SetRecordingButtons()
         {
             RecordingButtonDisplayer.Children.Clear();
             RecordingButtonDisplayer.Children.Add(Indent);
-            if(chosenRecordingNumber == -1)
             for (int i = 0; i < recordingList.Count; i++)
             {
                 var recordingButton = AddRecordingButton();
@@ -48,18 +54,13 @@ namespace MainWindow
         }
         private void ResetRecordingButtons(object sender, ListChangedEventArgs e)
         {
-            if(e.ListChangedType == ListChangedType.ItemAdded)
+            if (e.ListChangedType == ListChangedType.ItemAdded)
             {
-                AddRecordingButton();
+                RecordingButtonDisplayer.Children.Add(AddRecordingButton());
             }
             else if(e.ListChangedType == ListChangedType.ItemDeleted)
             {
-                RecordingButtonDisplayer.Children.RemoveAt(e.OldIndex);
-            }
-            else if(e.ListChangedType == ListChangedType.ItemChanged)
-            {
-                RecordingButtonDisplayer.Children.RemoveAt(e.NewIndex);
-                RecordingButtonDisplayer.Children.Add(AddRecordingButton());
+                RecordingButtonDisplayer.Children.RemoveAt(e.NewIndex+1); //zero element is indent
             }
         }
         private Button AddRecordingButton()
@@ -70,10 +71,11 @@ namespace MainWindow
                 HorizontalContentAlignment = HorizontalAlignment.Center,
                 Style = Application.Current.FindResource("MaterialDesignToolButton") as Style,
                 Foreground = Brushes.White,
-                Height = 31
+                Height = 35
             };
+            recordingButton.Click += OpenChosenRecordingEditor;
             return recordingButton;
-        }      
+        }
 
         private void OpenNewRecordingEditor(object sender, RoutedEventArgs e)
         {
@@ -84,76 +86,96 @@ namespace MainWindow
         }
         private void CloseNewRecordingEditor()
         {
+            if (IsRecordingEditorDisplayerOpen) { CloseRecordingEditorDisplayer(); }
             recordingEditor.IsEndWork -= CloseNewRecordingEditor;
             recordingEditor = null;
-            if (IsRecordingEditorDisplayerOpen) { CloseRecordingEditorDisplayer(); }
         }
-        private bool IsRecordingEditorDisplayerOpen = false;
-        private void OpenRecordingEditorDisplayer()
-        {
-            var animation = new DoubleAnimation(5, 80, TimeSpan.FromSeconds(0.5));
-            RecordingEditorDisplayer.BeginAnimation(HeightProperty, animation);
-            IsRecordingEditorDisplayerOpen = true;
-        }
+
         private void OpenChosenRecordingEditor(object sender, RoutedEventArgs e)
         {
-
+            if(chosenRecordingNumber != -1)
+            {               
+                RecordingButtonDisplayer.Children[chosenRecordingNumber+1].SetValue(ForegroundProperty, Brushes.White);
+            }
+            var chosenButton = sender as Button;
+            int recordingIndex = RecordingButtonDisplayer.Children.IndexOf(chosenButton)-1; //zero element is indent
+            if(recordingIndex == chosenRecordingNumber)
+            {
+                CloseChosenRecordingEditor();
+                IsRecordingEditorDisplayerOpen = false;
+                return;
+            }
+            chosenRecordingNumber = recordingIndex;
+            Recording chosenRecording = recordingList[recordingIndex];
+            recordingEditor = new ChosenRecordingEditor(ref chosenRecording);
+            recordingEditor.IsEndWork += CloseChosenRecordingEditor;
+            chosenButton.Foreground = Brushes.Red;
+            RecordingEditorDisplayer.Child = recordingEditor.body;
+            if (!IsRecordingEditorDisplayerOpen) { OpenRecordingEditorDisplayer(); }
         }
         private void CloseChosenRecordingEditor()
         {
-
+            ChosenRecordingEditor currentEditorState = (ChosenRecordingEditor)recordingEditor;
+            if (currentEditorState.ChosenRecording == null)
+            {
+                DeleteRecordingInRecordingList();
+            }
+            else
+            {
+                ChangeRecordingInRecordingList(currentEditorState.ChosenRecording);
+            }
+            if (IsRecordingEditorDisplayerOpen) { CloseRecordingEditorDisplayer(); }
+            chosenRecordingNumber = -1;
+            recordingEditor.IsEndWork -= CloseChosenRecordingEditor;
+            recordingEditor = null;
+        }
+        private void ChangeRecordingInRecordingList(Recording recording)
+        {
+            recordingList.RemoveAt(chosenRecordingNumber);
+            recordingList.Add(recording);
+        }
+        private void DeleteRecordingInRecordingList()
+        {
+            deletedRecorgingList.Add(recordingList[chosenRecordingNumber]);
+            recordingList.RemoveAt(chosenRecordingNumber);
+        }
+        
+        private bool IsRecordingEditorDisplayerOpen = false;
+        private void OpenRecordingEditorDisplayer()
+        {
+            MainWindowDisplayer.IsHitTestVisible = false;
+            var animation = new DoubleAnimation(5, 80, TimeSpan.FromSeconds(0.5));
+            animation.Completed += UnblockClick;
+            RecordingEditorDisplayer.BeginAnimation(HeightProperty, animation);
+            IsRecordingEditorDisplayerOpen = true;
         }
         private void CloseRecordingEditorDisplayer()
         {
+            MainWindowDisplayer.IsHitTestVisible = false;
             var animation = new DoubleAnimation(80, 5, TimeSpan.FromSeconds(0.5));
+            animation.Completed += UnblockClick;
             RecordingEditorDisplayer.BeginAnimation(HeightProperty, animation);
             IsRecordingEditorDisplayerOpen = false;
         }
-
-        private PasswordGenerator passwordGenerator;
-        private ButtonCondition PasswordGeneratorButtonCondition = ButtonCondition.Closure;
-
-        private void PasswordGeneratorButton_Click(object sender, RoutedEventArgs e)
+        private void UnblockClick(object sender, EventArgs e)
         {
-            if (PasswordGeneratorButtonCondition == ButtonCondition.Closure)
-            {
-                passwordGenerator = new PasswordGenerator();
-                passwordGenerator.Closed += PasswordGenerator_Closed;
-                passwordGenerator.Show();
-                PasswordGeneratorButton.Content = "Закрыть генератор пароля";
-            }
-            else if (PasswordGeneratorButtonCondition == ButtonCondition.Opened)
-            {
-                passwordGenerator.Closed -= PasswordGenerator_Closed;
-                passwordGenerator.Close();
-                PasswordGeneratorButton.Content = "Генератор пароля";
-            }
-            ChangeButtonCondition(ref PasswordGeneratorButtonCondition);
-        }
-        private void PasswordGenerator_Closed(object sender, EventArgs e)
-        {
-            PasswordGeneratorButton.Content = "Генератор пароля";
-            ChangeButtonCondition(ref PasswordGeneratorButtonCondition);
+            MainWindowDisplayer.IsHitTestVisible = true;
         }
 
-        private void ChangeButtonCondition(ref ButtonCondition button)
-        {
-            switch (button)
-            {
-                case ButtonCondition.Opened:
-                    button = ButtonCondition.Closure;
-                    return;
-                case ButtonCondition.Closure:
-                    button = ButtonCondition.Opened;
-                    return;
-            }
-        }
         private class NewRecordingEditor : RecordingEditor
         {
+            private Recording _newRecording;
             private BindingList<Recording> _recordingList;
             private TextBox NameField;
             private TextBox PasswordField;
-            public NewRecordingEditor(ref BindingList<Recording> recordingList) : base()
+            private Recording NewRecording
+            {
+                get
+                {
+                    return _newRecording;
+                }
+            }
+            public NewRecordingEditor(ref BindingList<Recording> recordingList)
             {
                 _recordingList = recordingList;
                 NameField = AddTextBox("Название записи");
@@ -183,7 +205,64 @@ namespace MainWindow
         }
         private class ChosenRecordingEditor : RecordingEditor
         {
-
+            private Recording _chosenRecording;
+            private TextBox PasswordField;
+            public Recording ChosenRecording 
+            {
+                get
+                {
+                    return _chosenRecording;
+                } 
+            }
+            public ChosenRecordingEditor(ref Recording chosenRecording)
+            {
+                _chosenRecording = chosenRecording;
+                PasswordField = AddTextBox(chosenRecording.Password);
+                AddButton("Скопировать", CopyRecording_Click, 150);
+                AddButton("Изменить", ChangeRecording_Click, 150);
+                AddButton("Изменить и скопировать", ChangeAndCopyRecording_Click, 300);
+                AddButton("Удалить", DeleteRecording_Click, 150);
+                AddButton("Закрыть", CloseEditor_Click, 150);
+            }
+            private void CopyRecording_Click(object sender, RoutedEventArgs e)
+            {
+                Clipboard.SetText(PasswordField.Text);
+            }
+            private void ChangeRecording_Click(object sender, RoutedEventArgs e)
+            {
+                if (!AreChosenRecordingAndPasswordFieldPasswordSame())
+                {
+                    _chosenRecording.UpdatePassword(PasswordField.Text);
+                    Close();
+                }
+            }
+            private void ChangeAndCopyRecording_Click(object sender, RoutedEventArgs e)
+            {
+                if (!AreChosenRecordingAndPasswordFieldPasswordSame())
+                {
+                    Clipboard.SetText(PasswordField.Text);
+                    _chosenRecording.UpdatePassword(PasswordField.Text);
+                    Close();
+                }
+            }
+            private bool AreChosenRecordingAndPasswordFieldPasswordSame()
+            {
+                if (_chosenRecording.Password == PasswordField.Text)
+                {
+                    MessageBox.Show("Новый пароль должен отличаться от старого");
+                    return true;
+                }
+                return false;
+            }
+            private void DeleteRecording_Click(object sender, RoutedEventArgs e)
+            {
+                _chosenRecording = null;
+                Close();
+            }
+            private void CloseEditor_Click(object sender, RoutedEventArgs e)
+            {
+                Close();
+            }
         }
         private abstract class RecordingEditor
         {
@@ -232,6 +311,45 @@ namespace MainWindow
                 body.ColumnDefinitions.Add(new ColumnDefinition());
                 body.Children.Add(container);
                 return textBox;
+            }
+        }
+
+
+        private PasswordGenerator passwordGenerator;
+        private ButtonCondition PasswordGeneratorButtonCondition = ButtonCondition.Closure;
+        private void PasswordGeneratorButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (PasswordGeneratorButtonCondition == ButtonCondition.Closure)
+            {
+                passwordGenerator = new PasswordGenerator();
+                passwordGenerator.Closed += PasswordGenerator_Closed;
+                passwordGenerator.Show();
+                PasswordGeneratorButton.Content = "Закрыть генератор пароля";
+            }
+            else if (PasswordGeneratorButtonCondition == ButtonCondition.Opened)
+            {
+                passwordGenerator.Closed -= PasswordGenerator_Closed;
+                passwordGenerator.Close();
+                PasswordGeneratorButton.Content = "Генератор пароля";
+            }
+            ChangeButtonCondition(ref PasswordGeneratorButtonCondition);
+        }
+        private void PasswordGenerator_Closed(object sender, EventArgs e)
+        {
+            PasswordGeneratorButton.Content = "Генератор пароля";
+            ChangeButtonCondition(ref PasswordGeneratorButtonCondition);
+        }
+
+        private void ChangeButtonCondition(ref ButtonCondition button)
+        {
+            switch (button)
+            {
+                case ButtonCondition.Opened:
+                    button = ButtonCondition.Closure;
+                    return;
+                case ButtonCondition.Closure:
+                    button = ButtonCondition.Opened;
+                    return;
             }
         }
     }
